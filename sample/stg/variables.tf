416984d8c3d545ed80466ffff75cc724ffcee6d2
Tetsuo NAKAMURA <tetsuo.nakamura@i2ts.com>
2019-04-16 17:58:06 +0900

    Small fix

--- sample/stg/variables.tf
+++ sample/stg/variables.tf
@@ -2,7 +2,7 @@ variable "label" {
   default = {
     namespace   = "nakt"    # Namespace, which could be your organization name
     environment = "private" # Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'
-    stage       = "prd"     # Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'
+    stage       = "stg"     # Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'
     name        = "ec2"     # Solution name, e.g. 'app' or 'jenkins'
     delimiter   = "-"       # Delimiter to be used between name, namespace, stage, etc.
   }
@@ -17,7 +17,7 @@ variable "tags" {
 variable "instance" {
   default = {
     count         = "10"
-    instance_type = "t3.large"
+    instance_type = "t3.micro"
   }
 }
 

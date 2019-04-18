b7e4eaafc6d2b01618e7e48971029d608f2cd98a
Tetsuo NAKAMURA <tetsuo.nakamura@i2ts.com>
2019-04-18 10:15:24 +0900

    Added Amazon Linux ami

--- modules/amisearch/main.tf
+++ modules/amisearch/main.tf
@@ -27,3 +27,17 @@ data "aws_ami" "ubuntu" {
     values = ["gp2"]
   }
 }
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
--- modules/amisearch/output.tf
+++ modules/amisearch/output.tf
@@ -1,3 +1,7 @@
 output "ubuntu" {
   value = "${data.aws_ami.ubuntu.id}"
 }
+
+output "amazon_linux_2" {
+  value = "${data.aws_ami.amazon_linux_2.id}"
+}
